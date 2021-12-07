141bfe4f19830b557257c534daac8c77a73bbb6b
kurtreyn <kurtreyn@yahoo.com>
2021-12-06 19:00:48 -0500

    finished js for modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,9 +65,6 @@ function addOverlay() {
       createOverlay(code, page);
       imgCol[i].appendChild(leftOverlay);
       imgCol[i].appendChild(rightOverlay);
-    } else {
-      // imgCol[i].removeChild(leftOverlay);
-      // imgCol[i].removeChild(rightOverlay);
     }
   }
 }
@@ -93,19 +90,62 @@ for (const ic of imgCol) {
 const modalWindow = document.querySelector('#modal-window');
 const closeModalBtn = document.querySelector('.modal-close-button');
 const modalTitle = document.querySelector('#modal-window-title');
-const modalImg = document.querySelector('.modal-img');
+const modalImg = document.querySelector('.modal-img-src');
 const modalText = document.querySelector('.modal-text');
+const modalCodeBtn = document.querySelector('.modal-code-btn');
+const modalPageBtn = document.querySelector('.modal-page-btn');
+const container = document.querySelector('.container');
+
+const sourceList = {
+  nostalgiaImg: './assets/images/know-nostalgia-960x540.jpg',
+  nostalgiaTitle: 'Know(Nostalgia)',
+  nostalgiaText: `Created as a coding bootcamp project. The site was built using Bootstrap, SCSS, and vanilla JavaScript. One highlight of the site is that it utilizes an API to access a movie database to allow users to search for movies. I have limited the search results to only display movies that were released between 1990 - 2009, as those are the decades the site is dedicated to.`,
+  ideaImg: './assets/images/iDea-thumnail-960x540.jpg',
+  ideaTitle: 'i.Dea Photography',
+  ideaText: `A site made for a ficticious photography studio. This is the first site I made using Bootstrap. One of the highlights of this site is a custom made lightbox on the gallery page, which is controlled with jQuery.`,
+  calcImg: './assets/images/calcApp-960x540.jpg',
+  calcTitle: 'Calculator App',
+  calcText: `This app was built with vanilla JavaScript, HTML & CSS.`,
+  storyImg: './assets/images/storyteller-thumbnail-960x540.jpg',
+  storyTitle: 'Storyteller Studios',
+  storyText: `This is the first site I made when I started to learn coding. Made with HTML and CSS. The site is for my small film production group, Storyteller Studios.`,
+};
 
 function showModal() {
   let selection = this.getAttribute('data-div-info');
-  console.log(selection);
+  // console.log(selection);
+  container.classList.add('blur');
 
-  // switch (selection) {
-  //   case 'nostalgia':
-  //     modalImg = './assets/images/know-nostalgia-960x540.jpg';
-  //     modalText = 'text';
-  //     break;
-  // }
+  switch (selection) {
+    case 'nostalgia':
+      modalImg.src = sourceList.nostalgiaImg;
+      modalTitle.innerText = sourceList.nostalgiaTitle;
+      modalText.innerText = sourceList.nostalgiaText;
+      modalCodeBtn.href = linkList.nostalgiaCode;
+      modalPageBtn.href = linkList.nostalgiaPage;
+      break;
+    case 'idea':
+      modalImg.src = sourceList.ideaImg;
+      modalTitle.innerText = sourceList.ideaTitle;
+      modalText.innerText = sourceList.ideaText;
+      modalCodeBtn.href = linkList.ideaCode;
+      modalPageBtn.href = linkList.ideaPage;
+      break;
+    case 'calc':
+      modalImg.src = sourceList.calcImg;
+      modalTitle.innerText = sourceList.calcTitle;
+      modalText.innerText = sourceList.calcText;
+      modalCodeBtn.href = linkList.calcCode;
+      modalPageBtn.href = linkList.calcPage;
+      break;
+    case 'story':
+      modalImg.src = sourceList.storyImg;
+      modalTitle.innerText = sourceList.storyTitle;
+      modalText.innerText = sourceList.storyText;
+      modalCodeBtn.href = linkList.storyCode;
+      modalPageBtn.href = linkList.storyPage;
+      break;
+  }
 
   modalWindow.style.display = 'block';
   modalWindow.classList.add('show');
@@ -114,6 +154,7 @@ function showModal() {
 function closeModal() {
   modalWindow.style.display = 'none';
   modalWindow.classList.remove('show');
+  container.classList.remove('blur');
 }
 
 for (const img of imgCol) {
